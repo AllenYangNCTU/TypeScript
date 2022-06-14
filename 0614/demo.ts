84e295bad9c5de21714757c18207470b455adcd8
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-06-14 23:31:07 +0800

    聯合類型＆＆類型保護

---
+++ 0614/demo.ts
@@ -0,0 +1,43 @@
+interface Bird {
+  fly: boolean;
+  sing: () => {};
+}
+
+interface Dog {
+  fly: boolean;
+  bark: () => {};
+}
+
+function trainAnimal(animal: Bird | Dog) {
+  if (animal.fly) {
+    (animal as Bird).sing();
+  } else {
+    (animal as Dog).bark();
+  }
+}
+
+function trainAnimal2(animal: Bird | Dog) {
+  if ("sing" in animal) {
+    animal.sing();
+  } else {
+    animal.bark();
+  }
+}
+
+function add(first: string | number, second: string | number) {
+  if (typeof first == "string" || typeof second == "string") {
+    return `${first}${second}`;
+  }
+  return first + second;
+}
+
+class NumberObj {
+  count: number = 0;
+}
+
+function addSecond(first: object | NumberObj, second: object | NumberObj) {
+  if (first instanceof NumberObj && second instanceof NumberObj) {
+    return first.count + second.count;
+  }
+  return 1;
+}
