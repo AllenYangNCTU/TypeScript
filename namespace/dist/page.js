dd7bf1345779b7f24877dccf3f0a4b55382da134
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-06-24 12:24:05 +0800

    namespace

---
+++ namespace/dist/page.js
@@ -0,0 +1,33 @@
+"use strict";
+var Home;
+(function (Home) {
+    class Header {
+        constructor() {
+            const elem = document.createElement("div");
+            elem.innerText = "This is a Header";
+            document.body.appendChild(elem);
+        }
+    }
+    class Content {
+        constructor() {
+            const elem = document.createElement("div");
+            elem.innerText = "This is a Content";
+            document.body.appendChild(elem);
+        }
+    }
+    class Footer {
+        constructor() {
+            const elem = document.createElement("div");
+            elem.innerText = "This is a Footer";
+            document.body.appendChild(elem);
+        }
+    }
+    class Page {
+        constructor() {
+            new Header();
+            new Content();
+            new Footer();
+        }
+    }
+    Home.Page = Page;
+})(Home || (Home = {}));
